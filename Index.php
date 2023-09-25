e71b1f76107cee50b025a829151356bdc8bbf92a
Yuu-Gewo <145958938+Yuu-Gewo@users.noreply.github.com>
2023-09-25 14:07:27 +0800

    Add files via upload

---
+++ Index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link rel="stylesheet" href="CSS/IndexStyle.css">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Quiz</title>
+</head>
+
+<body>
+    <form action="Question.php" method="post">
+        <div class="box">
+            <h1 class="label">Hello and Welcome!</h1>
+            <input type="submit" value="Start Quiz">
+        </div>
+    </form>
+</body>
+</html>
+
